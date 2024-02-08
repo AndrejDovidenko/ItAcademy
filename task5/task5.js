22442cc81f75653bbde99e1be72445f965a2bb2e
AndrejDovidenko <andrejdovidenko@gmail.com>
2024-02-08 16:41:36 +0300

    fix task5

--- task5/task5.js
+++ task5/task5.js
@@ -27,12 +27,27 @@ function uncompress(str) {
   if (str.length === 1) {
     return console.log(str);
   }
-  str
-    .split("")
-    .forEach((el, i) => (+el ? (result += str[i - 1].repeat(+str[i])) : null));
+
+  let arr = [];
+  let subStr = "";
+
+  str.split("").forEach((el, i) => {
+    if (isNaN(el)) {
+      arr.push(el);
+    }
+    if (!isNaN(str[i + 1])) {
+      subStr += str[i + 1];
+    } else {
+      arr.push(subStr);
+      subStr = "";
+    }
+  });
+
+  arr.forEach((el, i) => (+el ? (result += arr[i - 1].repeat(+arr[i])) : null));
 
   return console.log(result);
 }
 
 compress("dddgggssjsii");
 uncompress("d3g3s2j1s1i2");
+uncompress("d1a10c100f1");
